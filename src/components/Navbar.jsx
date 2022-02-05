d2a0d5819c76fd4367208b32b2286d4ba9c56c78
Nuwa-Hub <you@example.com>
2022-02-05 17:57:03 +0530

    change

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+	height: 60px;
+	background-color: black;
+`;
+
+const Wrapper = styled.div`
+	height: 60px;
+    width:60px;
+	
+`;
+
+const Navbar = () => {
+	return <Container></Container>;
+};
+
+export default Navbar;
